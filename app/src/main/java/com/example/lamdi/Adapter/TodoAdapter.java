366bff8ba737218703b5eb294179a243d7289bd7
hungnguyen18 <hungtrewq3214@gmail.com>
2024-03-25 22:31:30 +0700

    feat: update bottomsheet

--- app/src/main/java/com/example/lamdi/Adapter/TodoAdapter.java
+++ app/src/main/java/com/example/lamdi/Adapter/TodoAdapter.java
@@ -1,24 +1,31 @@
 package com.example.lamdi.Adapter;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.lamdi.AddNewTask;
 import com.example.lamdi.MainActivity;
 import com.example.lamdi.Model.TodoModel;
 import com.example.lamdi.R;
+import com.example.lamdi.Utils.DatabaseHandler;
 
 import java.util.List;
 
 public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
     private List<TodoModel> todoList;
     private MainActivity activity;
+    private DatabaseHandler db;
 
-    public TodoAdapter(MainActivity activity) {
+    public TodoAdapter(DatabaseHandler db, MainActivity activity) {
+        this.db = db;
         this.activity =activity;
     }
 
@@ -29,9 +36,20 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
     }
 
     public void onBindViewHolder(ViewHolder holder, int position) {
+        db.openDatabase();
         TodoModel item = todoList.get(position);
         holder.task.setText(item.getTask());
         holder.task.setChecked(toBoolean(item.getStatus()));
+        holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    db.updateStatus(item.getId(), 1);
+                } else  {
+                    db.updateStatus(item.getId(), 0);
+                }
+            }
+        });
     }
 
     public int getItemCount() {
@@ -45,6 +63,27 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
     public void setTasks(List<TodoModel> todoList) {
         this.todoList = todoList;
         notifyDataSetChanged();
+    }
+
+    public Context getContext() {
+        return activity;
+    }
+
+    public void deleteItem(int position) {
+        TodoModel item = todoList.get(position);
+        db.deleteTask(item.getId());
+        todoList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void editItem(int position) {
+        TodoModel item = todoList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", item.getId());
+        bundle.putString("task", item.getTask());
+        AddNewTask fragment = new AddNewTask();
+        fragment.setArguments(bundle);
+        fragment.show(activity.getSupportFragmentManager(), AddNewTask.TAG);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/com/example/lamdi/MainActivity.java
+++ app/src/main/java/com/example/lamdi/MainActivity.java
@@ -4,43 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.lamdi.Adapter.TodoAdapter;
 import com.example.lamdi.Model.TodoModel;
+import com.example.lamdi.Utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogCloseListener {
     private RecyclerView tasksRecyclerView;
     private TodoAdapter tasksAdapter;
     private List<TodoModel> taskList;
+    private DatabaseHandler db;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
         taskList = new ArrayList<>();
 
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        tasksAdapter = new TodoAdapter(this);
+        tasksAdapter = new TodoAdapter(db, MainActivity.this);
         tasksRecyclerView.setAdapter(tasksAdapter);
 
-        TodoModel task = new TodoModel();
-        task.setTask("This is a test");
-        task.setStatus(0);
-        task.setId(1);
+        fab = findViewById(R.id.fab);
 
-        taskList.add(task);
-        taskList.add(task);
-        taskList.add(task);
-        taskList.add(task);
-        taskList.add(task);
-        taskList.add(task);
-
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
         tasksAdapter.setTasks(taskList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog) {
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
     }
 }
